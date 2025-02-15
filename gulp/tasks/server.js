656633b38314ba31dbecf76760899867c7936fc0
otriayd <otriaydmoney@gmail.com>
2025-02-15 18:39:59 +0300

    before header

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -2,7 +2,7 @@ export const server = (done) => {
 	app.plugins.browsersync.init({
 		server: {
 			baseDir: `${app.path.build.html}`,
-			index: 'product.html'
+			index: 'cart.html'
 		},
 		notify: true,
 		port: 3000,
